2dfe73eea19ed2c219a83cbb6e0cfc07f728d01d
Zanardi <zanardimz@gmail.com>
2022-04-08 22:01:45 -0300

    Implementação do BottomNavigation e ToolBar

--- app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
+++ app/src/main/java/br/senac/aula09examples/bottomnavigation/BottomNavigationActivity.kt
@@ -2,7 +2,11 @@ package br.senac.aula09examples.bottomnavigation
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula09examples.R
 import br.senac.aula09examples.databinding.ActivityBottomNavigationBinding
+import br.senac.aula09examples.fragments.AlbumsFragment
+import br.senac.aula09examples.fragments.ArtistsFragment
+import br.senac.aula09examples.fragments.RecentsFragment
 
 class BottomNavigationActivity : AppCompatActivity() {
 
@@ -13,6 +17,24 @@ class BottomNavigationActivity : AppCompatActivity() {
         binding = ActivityBottomNavigationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.artistas -> {
+                    val frag = ArtistsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                R.id.albuns -> {
+                    val frag = AlbumsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+                else -> {
+                    val frag = RecentsFragment()
+                    supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+                }
+            }
+            
+            true
+        }
     }
 
 }
--- app/src/main/java/br/senac/aula09examples/toolbarmenu/ToolbarMenuActivity.kt
+++ app/src/main/java/br/senac/aula09examples/toolbarmenu/ToolbarMenuActivity.kt
@@ -1,8 +1,14 @@
 package br.senac.aula09examples.toolbarmenu
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import br.senac.aula09examples.R
 import br.senac.aula09examples.databinding.ActivityToolbarMenuBinding
+import br.senac.aula09examples.fragments.AlbumsFragment
+import br.senac.aula09examples.fragments.ArtistsFragment
+import br.senac.aula09examples.fragments.RecentsFragment
 
 class ToolbarMenuActivity : AppCompatActivity() {
     lateinit var binding: ActivityToolbarMenuBinding
@@ -11,6 +17,29 @@ class ToolbarMenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityToolbarMenuBinding.inflate(layoutInflater)
         setContentView(binding.root)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbarmenu,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.artistas -> {
+                val frag = ArtistsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+            }
+            R.id.albuns -> {
+                val frag = AlbumsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+            }
+            else -> {
+                val frag = RecentsFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.container, frag).commit()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
